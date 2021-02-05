5619ecfa77bc2edce22bf77c3538f1562dbb8aa5
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-02-05 18:55:33 +0100

    add eslint

--- build_scripts/start_message.js
+++ build_scripts/start_message.js
@@ -1,3 +1,4 @@
-import { green } from 'chalk';
+import chalk from 'chalk';
 
-console.log(green('Starting app in dev mode...'));
+console.log(chalk.magenta('Starting dev server...'));
+setTimeout(function() { console.log(chalk.green('Running app on localhost:3000')); }, 3000);

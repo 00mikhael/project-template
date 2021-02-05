f4e1f5e00e2e749ec9a34a46be3a32ecf9ac775c
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-02-05 19:22:20 +0100

    complete lint setup

--- build_scripts/start_message.js
+++ build_scripts/start_message.js
@@ -1,4 +1,6 @@
 import chalk from 'chalk';
 
+/* eslint-disable no-console */
+
 console.log(chalk.magenta('Starting dev server...'));
 setTimeout(function() { console.log(chalk.green('Running app on localhost:3000')); }, 3000);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import numeral from 'numeral';
 import './index.css';
 
 const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome template!`);
+console.log(`I would pay ${courseValue} for this awesome template!`); // eslint-disable-line no-console
 document.getElementById('text').textContent = `Hello World!`;

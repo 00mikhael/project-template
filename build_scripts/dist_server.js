980a2248334a2b847546c9dd8f3469505e847a64
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-02-07 09:45:20 +0100

    add surge

--- build_scripts/dist_server.js
+++ build_scripts/dist_server.js
@@ -15,34 +15,6 @@ app.get('/', function(req, res) {
     res.sendFile(join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', function(req, res) {
-    res.json([{
-            "id": 25415276,
-            "firstName": "Quinton",
-            "lastName": "Moore",
-            "email": "Jazlyn67@hotmail.com"
-        },
-        {
-            "id": 8961012,
-            "firstName": "Jean",
-            "lastName": "McCullough",
-            "email": "Darby_OConner@yahoo.com"
-        },
-        {
-            "id": 44010415,
-            "firstName": "Delaney",
-            "lastName": "Boehm",
-            "email": "Rebecca_Schaefer44@gmail.com"
-        },
-        {
-            "id": 14917352,
-            "firstName": "Nia",
-            "lastName": "Pollich",
-            "email": "Colten_Bednar@gmail.com"
-        }
-    ]);
-});
-
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
